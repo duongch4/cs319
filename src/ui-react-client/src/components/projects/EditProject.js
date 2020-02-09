13f2b26483e0b716f1b38807f5bd94f1eb648c85
Lisa <lisaobrien2009@hotmail.com>
2020-02-08 19:33:57 -0800

    edit page stub added

---
+++ src/ui-react-client/src/components/projects/EditProject.js
@@ -0,0 +1,76 @@
+import React,{ Component } from 'react';
+import Openings from './Openings'
+import CreateEditProjectDetails from './CreateEditProjectDetails'
+import TeamRequirements from './TeamRequirements'
+import { updateProject, loadProjects } from '../../redux/actions/projectsActions.js';
+import { loadDisciplines } from '../../redux/actions/disciplinesActions.js';
+import { loadLocations} from '../../redux/actions/locationsActions.js';
+import { loadExperiences } from '../../redux/actions/experienceActions.js';
+import { connect } from 'react-redux';
+
+class EditProject extends Component {
+  state = {
+      // TODO: Depending how you implement this, you may or may not need the following lines:
+    // project: {
+    //   projID: null,
+    //   name: "",
+    //   location: {
+    //     city: "",
+    //     province: ""
+    //   },
+    //   startDate: "",
+    //   endDate: "",
+    //   openings: [],      
+    // }
+  }
+
+  componentDidMount(){
+    this.props.loadDisciplines();
+    // this.props.disciplines holds the master disciplines Map now
+    this.props.loadLocations();
+    // this.props.locations hold the master locations now
+    this.props.loadExperiences();
+    // this.props.masterYearsOfExperience holds the master list of experiences
+    this.props.loadProjects()
+    var currentProject = this.props.projects.filter(project => project.projID == this.props.match.params.project_id)
+    if(currentProject){
+        this.setState({
+            project: currentProject[0]
+            // state now holds the current project
+        })
+    }
+  }
+
+  onSubmit = () => {
+     this.props.updateProject(this.state.project)
+  }
+  
+  render(){
+      return (
+      <>
+      </>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    disciplines: state.disciplines,
+    locations: state.locations,
+    masterYearsOfExperience: state.masterYearsOfExperience,
+    projects: state.projects
+  };
+};
+
+const mapDispatchToProps = {
+  loadDisciplines,
+  loadLocations,
+  updateProject,
+  loadExperiences,
+  loadProjects
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(EditProject);
