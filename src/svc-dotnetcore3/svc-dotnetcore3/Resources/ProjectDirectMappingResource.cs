2e626cb6beba780f99063f905eb54c88e96ce8d4
jenessa <jxxtan@students.cs.ubc.ca>
2020-02-08 23:20:41 -0800

    created resources

---
+++ src/svc-dotnetcore3/svc-dotnetcore3/Resources/ProjectDirectMappingResource.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Web.API.Resources
+{
+    public class ProjectDirectMappingResource
+    {
+        /// <summary>Project ID</summary>
+        /// <example>3570</example>
+        public int Id { get; set; }
+        /// <summary>Project Name</summary>
+        /// <example>Designing the new Pawnee Commons</example>
+        public string Name { get; set; }
+        /// <summary>Location ID</summary>
+        /// <example>5</example>
+        public int LocationId {get; set;}        
+        /// <summary>Project Start Date</summary>
+        /// <example>05/29/2021 05:50:06</example>
+        public DateTime ProjectStartDate {get; set;}
+        /// <summary>Project End Date</summary>
+        /// <example>01/29/2022 05:50:06</example>
+        public DateTime ProjectEndDate {get; set;}
+    }
+}
