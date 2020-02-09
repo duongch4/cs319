c59778b57841183e32ac50015809113b1e85ebf0
jenessa <jxxtan@students.cs.ubc.ca>
2020-02-08 22:46:31 -0800

    added new models based on DB schema

---
+++ src/svc-dotnetcore3/svc-dotnetcore3/Application/Models/ResourceDisciplines.cs
@@ -0,0 +1,10 @@
+namespace Web.API.Application.Models
+{
+    public class ResourceDisciplines
+    {
+        public int DisciplineId { get; set; }
+        public int ResourceId {get; set;}
+        public int YearsOfExperience {get; set;}
+
+    }
+}
