5000e65228916b7958a1748014b097c9b79249a3
Bang Chi Duong <chibangduong1993@gmail.com>
2020-02-09 20:22:57 -0800

    Cloud DB is configured; Now Testing if the cloud DB is populated by this repo

--- src/ui-react-client/src/config/config.js
+++ src/ui-react-client/src/config/config.js
@@ -1,6 +1,3 @@
-// const env = dotenv.config({ path: envPath }).parsed;
-
-
 export const TENANT_ID = process.env.REACT_APP_TENANT_ID;
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const SVC_ROOT = process.env.REACT_APP_SVC_ROOT;
+export const SVC_ROOT = process.env.REACT_APP_SVC_ROOT ? process.env.REACT_APP_SVC_ROOT : window.location.origin;
