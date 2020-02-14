81b8a3438a794e674fcd46d6aeb44b7cd3a2c2a1
Jackie <sangxu.yin@alumni.ubc.ca>
2020-02-13 17:02:09 -0800

    Fixed Typos to Projects and Users tables and added check to ResourseSkill Table

--- src/svc-dotnetcore3/Database/dbo/Tables/ResourceSkill.sql
+++ src/svc-dotnetcore3/Database/dbo/Tables/ResourceSkill.sql
@@ -4,9 +4,10 @@ CREATE TABLE [dbo].[ResourceSkill]
 	[ResourceDisciplineId] [int] Not Null,
 	[SkillDisciplineId] [int] Not Null,
 	[SkillId] [int] Not Null,
-	CONSTRAINT [FK_ResourceSkill_ResourceDiscipline] FOREIGN KEY ([ResourceId], [DisciplineId]) 
-		REFERENCES [ResourceDiscipline] ([ResourceId], [ResourceDisciplineId]) ON DELETE CASCADE,
+	CONSTRAINT [FK_ResourceSkill_ResourceDiscipline] FOREIGN KEY ([ResourceId], [ResourceDisciplineId]) 
+		REFERENCES [ResourceDiscipline] ([ResourceId], [DisciplineId]) ON DELETE CASCADE,
 	CONSTRAINT [FK_ResourceSkill_Skills] FOREIGN KEY ([SkillDisciplineId], [SkillId]) 
 		REFERENCES [Skills] ([DisciplineId], [Id]),
-	CONSTRAINT [PK_ResourseSkill] PRIMARY KEY ([ResourceId], [ResourceDisciplineId], [SkillDisciplineId], [SkillId])
+	CONSTRAINT [PK_ResourseSkill] PRIMARY KEY ([ResourceId], [ResourceDisciplineId], [SkillDisciplineId], [SkillId]),
+	CONSTRAINT [CK_DisciplinesMatch] CHECK ([ResourceDisciplineId] = [SkillDisciplineId])
 )
