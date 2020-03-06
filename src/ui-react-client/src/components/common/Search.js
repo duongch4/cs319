eca65c7f2701569b889ef48fc0cd54b7f97ac6eb
Nicole <nicolekitner@gmail.com>
2020-03-06 14:53:04 -0800

    added locations to search page and changed colour of apply filters

--- src/ui-react-client/src/components/common/Search.js
+++ src/ui-react-client/src/components/common/Search.js
@@ -10,11 +10,14 @@ class Search extends Component {
         search: {
             discipline: null,
             skills: [],
+            searchWord: "",
+            province: null,
+            cities: [],
         },
         masterlist: this.props.masterlist,
         pending: true
     };
-
+    
     componentDidMount() {
         if (CLIENT_DEV_ENV) {
             this.props.loadMasterlists()
@@ -37,7 +40,24 @@ class Search extends Component {
     }
 
     handleChange = (e) => {
-      if (e.target.id === "skills") {
+      if (e.target.id === "city") {
+        var cities_arr = [...this.state.search.cities, e.target.value];
+        this.setState({
+            search: {
+                ...this.state.search,
+                cities: cities_arr
+            }
+        });
+      } else if (e.target.id === "province") {
+        let newCities = this.props.masterlist.locations[e.target.value];
+        this.setState({
+            search: {
+                ...this.state.search,
+                province: e.target.value,
+            },
+            cities: newCities
+        });
+    } else if (e.target.id === "skills") {
         var skills_arr = [...this.state.search.skills, e.target.value];
           this.setState({
             search: {
@@ -45,6 +65,13 @@ class Search extends Component {
               skills: skills_arr
             }
          });
+      } else if (e.target.id === "search") {
+        this.setState({
+          search: {
+            ...this.state.search,
+            searchWord: e.target.value
+          }
+        });
       } else {
         this.setState({
             search: {
@@ -79,15 +106,51 @@ class Search extends Component {
           skill_render.push(<option key={"skills_" + i} value={skill}>{skill}</option>)
       })
     }
+
+    console.log(this.state.masterlist.locations);
+
+    var provinces = this.props.masterlist.locations; 
+    var provinces_render = [];
+    var all_provinces_key = Array.from(Object.keys(provinces));
+    all_provinces_key.forEach((province, i) => {
+      provinces_render.push(<option key={"province_" + i} value={province}>{province}</option>)
+    });
+
+    var cities = [];
+    var cities_render = [];
+    if (this.state.search.province=== null){
+      cities_render = <option disabled>Please select a province</option>
+    } else {
+      cities = provinces[this.state.search.province];
+      cities.forEach((city, i) => {
+        cities_render.push(<option key={"cities_" + i} value={city}>{city}</option>)
+      })
+    }
+
     return (
       <div className="activity-container">
         <div className="form-section">
-          <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Search.." name="search">
-            </input>
-            <div className="form-row">
+          <h2 className="greenHeader">Search</h2>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-row">
+                <input className="input-box" type="text" id="search" placeholder="Search" onChange={this.handleChange}/>
+              </div>
+              <div id="filters">
+              <div className="form-row">
                   <div className="form-section opening">
-                      <div className="form-row">
+                  <div className="form-row"> Location
+                          <select className="input-box" defaultValue={'DEFAULT'}
+                                  id="province" onChange={this.handleChange}>
+                              <option value="DEFAULT" disabled>Province</option>
+                              {provinces_render}
+                          </select>
+                          <select className="input-box" defaultValue={'DEFAULT'}
+                                  id="city" onChange={this.handleChange}>
+                              <option value="DEFAULT" disabled>City</option>
+                              {cities_render}
+                          </select>
+                          </div>
+                      <div className="form-row"> Disciplines
                           <select className="input-box" defaultValue={'DEFAULT'}
                                   id="discipline" onChange={this.handleChange}>
                               <option value="DEFAULT" disabled>Discipline</option>
@@ -98,13 +161,14 @@ class Search extends Component {
                               <option value="DEFAULT" disabled>Skills</option>
                               {skill_render}
                           </select>
-                      </div>
-                      </div>
-                      </div>
+                          </div>
+                          </div>
+                          </div>
+              </div>
                       <Button variant="contained"
-                                style={{backgroundColor: "#87c34b", color: "#ffffff", size: "small"}}
+                                style={{backgroundColor: "#2c6232", color: "#ffffff", size: "small"}}
                                 disableElevation
-                                onClick={() => this.onSubmit()}>Search</Button>
+                                onClick={() => this.onSubmit()}>Apply Filters</Button>
                     </form>
         </div>
       </div>
