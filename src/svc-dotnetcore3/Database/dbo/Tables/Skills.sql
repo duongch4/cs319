a93427484f17be304beadf57ff6f6ef1098e3423
Jackie <sangxu.yin@alumni.ubc.ca>
2020-03-07 20:08:11 -0800

    Removed ON DELETE CASCADE for disciplines and skills

--- src/svc-dotnetcore3/Database/dbo/Tables/Skills.sql
+++ src/svc-dotnetcore3/Database/dbo/Tables/Skills.sql
@@ -3,6 +3,6 @@
 	[Id] INT NOT NULL,
 	[DisciplineId] INT NOT NULL,
 	[Name] NVARCHAR(100) NOT NULL,
-	CONSTRAINT [FK_Skills_Disciplines] FOREIGN KEY ([DisciplineId]) REFERENCES [Disciplines]([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_Skills_Disciplines] FOREIGN KEY ([DisciplineId]) REFERENCES [Disciplines]([Id]) ON DELETE NO ACTION,
 	CONSTRAINT [PK_Skills] PRIMARY KEY ([DisciplineId], [Id])
 )
