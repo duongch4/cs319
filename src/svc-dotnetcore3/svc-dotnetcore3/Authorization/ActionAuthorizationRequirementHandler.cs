5ced03c122ba9ebc328fed527fe4c23c9fcbbc47
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-17 12:51:06 -0700

    Added Authorization in compliant with OAuth AzureAD v2.0 flow; Working with Swagger for now; Break v1.0 on client-side ==> Fix this next

---
+++ src/svc-dotnetcore3/svc-dotnetcore3/Authorization/ActionAuthorizationRequirementHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.API.Authorization
+{
+    internal class ActionAuthorizationRequirementHandler : AuthorizationHandler<ActionAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            ActionAuthorizationRequirement requirement)
+        {
+            // Checks the user has a permission accepted for this action
+            string[] delegatedPermissions = context.User.FindAll(Claims.ScopeClaimType).Select(c => c.Value).ToArray();
+            string[] acceptedDelegatedPermissions = AuthorizedPermissions.DelegatedPermissionsForActions[requirement.Action];
+            string[] appPermissionsOrRoles = context.User.FindAll(Claims.AppPermissionOrRolesClaimType).Select(c => c.Value).ToArray();
+            string[] acceptedApplicationPermissions = AuthorizedPermissions.ApplicationPermissionsForActions[requirement.Action];
+
+            if (acceptedDelegatedPermissions.Any(accepted => delegatedPermissions.Contains(accepted)))
+            {
+                context.Succeed(requirement);
+            }
+            else if (acceptedApplicationPermissions.Any(accepted => appPermissionsOrRoles.Contains(accepted)))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/svc-dotnetcore3/svc-dotnetcore3/Authorization/AnyValidPermissionRequirementHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.API.Authorization
+{
+    internal class AnyValidPermissionRequirementHandler : AuthorizationHandler<AnyValidPermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AnyValidPermissionRequirement requirement)
+        {
+            // Checks caller has at least one valid permission
+            string[] delegatedPermissions = context.User.FindAll(Claims.ScopeClaimType).Select(c => c.Value).ToArray();
+            string[] allAcceptedDelegatedPermissions = DelegatedPermissions.All;
+            string[] appPermissionsOrRoles = context.User.FindAll(Claims.AppPermissionOrRolesClaimType).Select(c => c.Value).ToArray();
+            string[] allAcceptedApplicationPermissions = ApplicationPermissions.All;
+            if (delegatedPermissions.Any(p => allAcceptedDelegatedPermissions.Contains(p)))
+            {
+                // Caller has a valid delegated permission
+                // If your API has different user roles,
+                // this is where you would check that, before calling context.Succeed()
+                context.Succeed(requirement);
+            }
+            else if (appPermissionsOrRoles.Any(p => allAcceptedApplicationPermissions.Contains(p)))
+            {
+                // Caller has a valid application permission
+                context.Succeed(requirement);
+            }
+
+            // If we reached here without calling context.Succeed(),
+            // the call will fail with a 403 Forbidden
+            return Task.CompletedTask;
+        }
+    }
+}
