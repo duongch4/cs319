6212819122b5c64349a9acfcf1f603f25bb2762f
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-18 15:38:20 -0700

    Wrapped AuthProvider to the whole App so the logged-in user's info is accessible everywhere

--- src/ui-react-client/src/components/App.js
+++ src/ui-react-client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 //Import all needed Components
 import Header from './common/Header';
@@ -16,13 +16,17 @@ import Admin from './admin/Admin.js';
 
 import { CurrentUser } from "./users/CurrentUser.js";
 
-const App = () => {
+import AuthProvider from "../config/AuthProvider";
+import PropTypes from "prop-types";
+import { PropsRoute } from "../util/CustomRoute";
+
+const App = (appProps) => {
   return (
     <div>
-      <Header />
+      <Header appProps={appProps} />
       <Switch>
         {/*All our Routes goes here!*/}
-        <Route exact path="/" component={HomePage} />
+        <PropsRoute exact path="/" component={HomePage} profile={appProps.graphProfile} account={appProps.account} />
         <Route exact path="/users" component={UsersPage} />
         <Route exact path="/projects" component={ProjectsPage} />
         <Route path="/locations" component={LocationsPage} />
@@ -39,4 +43,14 @@ const App = () => {
   );
 };
 
-export default App;
+App.propTypes = {
+  account: PropTypes.object,
+  emailMessages: PropTypes.object,
+  error: PropTypes.string,
+  graphProfile: PropTypes.object,
+  onSignIn: PropTypes.func.isRequired,
+  onSignOut: PropTypes.func.isRequired,
+  onRequestEmailToken: PropTypes.func.isRequired
+};
+
+export default AuthProvider(App);
