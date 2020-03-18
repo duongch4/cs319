968466b1cb475968a0733e7c80edf3aca6b35197
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-18 01:43:35 -0700

    Restructure Header to have LogIn/LogOut functionality; Added CurrentUser Information Page for future usage --> check for Roles/Scopes: Will be removed when required!

--- src/ui-react-client/src/components/common/Header.js
+++ src/ui-react-client/src/components/common/Header.js
@@ -1,43 +1,59 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import UserHeaderMenu from './UserHeaderMenu';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => {
-  const activeStyle = { color: '#ffffff' };
-  const style={ color: '#ffffff', fontSize: '20px'};
-  return (
-    <div className="sidenav">
-      <p className="ae-logo">AE</p>
-      <p className="ae-subtitle">Associated Engineering</p>
-      <svg width={80} height={80}>
-        <circle r={40} cx={40} cy={40}></circle>
-      </svg>
-      <UserHeaderMenu />
-      <nav>
-        <div>
-        <NavLink to="/" style={style} activeStyle={activeStyle} exact>
-          Home
-        </NavLink>
-        </div>
-        <div>
-        <NavLink to="/users" style={style} activeStyle={activeStyle} exact>
-          Users
-        </NavLink>
-        </div>
-        <div>
-        <NavLink to="/projects" style={style} activeStyle={activeStyle} exact>
-          Projects
-        </NavLink>
-        </div>
-        <div>
-        <NavLink to="/admin" style={style} activeStyle={activeStyle} exact>
-          Admin
-        </NavLink>
-        </div>
-      </nav>
-    </div>
-  );
-};
+import PropTypes from "prop-types";
+import AuthProvider from "../../config/AuthProvider";
+import { isIE } from "../../config/authUtils";
+import { RequestButton } from "./RequestButton";
 
-export default Header;
+class Header extends React.Component {
+  static propTypes = {
+    account: PropTypes.object,
+    emailMessages: PropTypes.object,
+    error: PropTypes.string,
+    graphProfile: PropTypes.object,
+    onSignIn: PropTypes.func.isRequired,
+    onSignOut: PropTypes.func.isRequired,
+    onRequestEmailToken: PropTypes.func.isRequired
+  };
+
+  render() {
+    return (
+      <div className="sidenav">
+        <p className="ae-logo">AE</p>
+        <p className="ae-subtitle">Associated Engineering</p>
+        <svg width={80} height={80}>
+          <circle r={40} cx={40} cy={40}></circle>
+        </svg>
+        {
+          !this.props.account ? <RequestButton text="Log In" onClick={this.props.onSignIn} useRedirect={isIE()} /> : (
+            <div>
+              <RequestButton text="Log Out" onClick={this.props.onSignOut} />
+              <RequestButton text="Request Email Token" onClick={this.props.onRequestEmailToken} />
+              <nav className="navlink">
+                <div><Link to="/">Home</Link></div>
+                <div><Link to="/users">Users</Link></div>
+                <div><Link to="/projects">Projects</Link></div>
+                <div><Link to="/admin">Admin</Link></div>
+                <div><Link to={
+                  {
+                    pathname: "/current_user",
+                    state: {
+                      account: this.props.account,
+                      profile: this.props.graphProfile,
+                      emailMessages: this.props.emailMessages
+                    }
+                  }
+                }>Current User</Link></div>
+              </nav>
+            </div>
+          )
+        }
+        {this.props.error && (<p className="error">Error: {this.props.error}</p>)}
+      </div>
+    );
+  }
+}
+
+export default AuthProvider(Header);
