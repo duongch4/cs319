7ac16383b26c0c019ec520dbec0f290fa13190bf
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-18 01:37:01 -0700

    Removed react-adal; Added MSAL for AzureAD OAuth2 v2.0

--- src/ui-react-client/src/index.js
+++ src/ui-react-client/src/index.js
@@ -1,14 +1,22 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
-import { runWithAdal } from 'react-adal';
-import { authContext } from './config/adalConfig';
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import './components/common/common.css';
+import App from './components/App';
+import configureStore from './redux/configureStore';
+import { Provider } from 'react-redux';
 
-const DO_NOT_LOGIN = false;
+const store = configureStore();
 
-runWithAdal(authContext, () => {
-
-  // eslint-disable-next-line
-  require('./indexApp.js');
-
-},DO_NOT_LOGIN);
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
