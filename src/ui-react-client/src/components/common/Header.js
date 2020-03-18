0248c458ae30ceacf28dd9c7581a43976717a025
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-18 15:38:20 -0700

    Wrapped AuthProvider to the whole App so the logged-in user's info is accessible everywhere

--- src/ui-react-client/src/components/common/Header.js
+++ src/ui-react-client/src/components/common/Header.js
@@ -2,22 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-import PropTypes from "prop-types";
-import AuthProvider from "../../config/AuthProvider";
 import { isIE } from "../../config/authUtils";
 import { RequestButton } from "./RequestButton";
 
 class Header extends React.Component {
-  static propTypes = {
-    account: PropTypes.object,
-    emailMessages: PropTypes.object,
-    error: PropTypes.string,
-    graphProfile: PropTypes.object,
-    onSignIn: PropTypes.func.isRequired,
-    onSignOut: PropTypes.func.isRequired,
-    onRequestEmailToken: PropTypes.func.isRequired
-  };
-
   render() {
     return (
       <div className="sidenav">
@@ -27,10 +15,10 @@ class Header extends React.Component {
           <circle r={40} cx={40} cy={40}></circle>
         </svg>
         {
-          !this.props.account ? <RequestButton text="Log In" onClick={this.props.onSignIn} useRedirect={isIE()} /> : (
+          !this.props.appProps.account ? <RequestButton text="Log In" onClick={this.props.appProps.onSignIn} useRedirect={isIE()} /> : (
             <div>
-              <RequestButton text="Log Out" onClick={this.props.onSignOut} />
-              <RequestButton text="Request Email Token" onClick={this.props.onRequestEmailToken} />
+              <RequestButton text="Log Out" onClick={this.props.appProps.onSignOut} />
+              <RequestButton text="Request Email Token" onClick={this.props.appProps.onRequestEmailToken} />
               <nav className="navlink">
                 <div><Link to="/">Home</Link></div>
                 <div><Link to="/users">Users</Link></div>
@@ -40,9 +28,9 @@ class Header extends React.Component {
                   {
                     pathname: "/current_user",
                     state: {
-                      account: this.props.account,
-                      profile: this.props.graphProfile,
-                      emailMessages: this.props.emailMessages
+                      account: this.props.appProps.account,
+                      profile: this.props.appProps.graphProfile,
+                      emailMessages: this.props.appProps.emailMessages
                     }
                   }
                 }>Current User</Link></div>
@@ -50,10 +38,10 @@ class Header extends React.Component {
             </div>
           )
         }
-        {this.props.error && (<p className="error">Error: {this.props.error}</p>)}
+        {this.props.appProps.error && (<p className="error">Error: {this.props.appProps.error}</p>)}
       </div>
     );
   }
 }
 
-export default AuthProvider(Header);
+export default Header;
--- src/ui-react-client/src/components/home/HomePage.js
+++ src/ui-react-client/src/components/home/HomePage.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const HomePage = () => {
-  return (
+const Json = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>;
+
+const HomePage = (props) => {
+
+  if (!props.account) {
+    return (
       <div className="activity-container">
         <h1>Home</h1>
-        <p>Boilerplate project</p>
+        <h3>YOU ARE NOT LOGGED IN</h3>
       </div>
-  );
-};
+    );
+  }
+  else if (!props.profile) {
+    return (
+      <div className="activity-container">
+        <h1>Home</h1>
+        <h3>LOADING YOUR PROFILE</h3>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="activity-container">
+        <h1>Home</h1>
+        <h3>HERE IS YOUR PROFILE</h3>
+        <Json data={props.profile} />
+      </div>
+    );
+  }
+
+}
+
 export default HomePage;
