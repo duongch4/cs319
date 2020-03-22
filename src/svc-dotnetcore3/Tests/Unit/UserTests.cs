94048d171d35ca9cae031d708719d53cb0cebd54
Bang Chi Duong <chibangduong1993@gmail.com>
2020-03-22 12:42:58 -0700

    Fixed test for the changed data structure

--- src/svc-dotnetcore3/Tests/Unit/UserTests.cs
+++ src/svc-dotnetcore3/Tests/Unit/UserTests.cs
@@ -11,14 +11,14 @@ namespace Tests.Unit
         {
             var properties = new User
             {
-                Id = 1,
+                Id = "1",
                 FirstName = "John",
                 LastName = "Doe",
                 Username = "doej",
                 LocationId = 8
             };
 
-            properties.Id.Should().Be(1);
+            properties.Id.Should().Be("1");
             properties.FirstName.Should().Be("John");
             properties.LastName.Should().Be("Doe");
             properties.LocationId.Should().Be(8);
