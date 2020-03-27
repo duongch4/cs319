b84ee68a3c84634928f2b78f25aa977492416b30
jenessa <jxxtan@students.cs.ubc.ca>
2020-03-27 14:44:42 -0700

    user repo setup

--- src/svc-dotnetcore3/Tests/Unit/UsersControllerUnitTests.cs
+++ src/svc-dotnetcore3/Tests/Unit/UsersControllerUnitTests.cs
@@ -45,5 +45,119 @@ namespace Tests.Unit
             );
 
         }
+
+        /********** Helper functions for verification **********/
+
+        /********** Helper function for User repo setup **********/
+        private void Setup_UsersRepo_GetAllUserResourcesOnFilter_ThrowsException(System.Exception exception)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAllUserResourcesOnFilter(It.IsAny<RequestSearchUsers>())
+            ).Throws(exception);
+        }
+
+        private void Setup_UsersRepo_GetAllUserResourcesOnFilter_Default(IEnumerable<UserResource> returnVal)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAllUserResourcesOnFilter(It.IsAny<RequestSearchUsers>())
+            ).ReturnsAsync(returnVal);
+        }
+
+        private void Setup_UsersRepo_GetAllUserResources_ThrowsException(System.Exception exception)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAllUserResources(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>())
+            ).Throws(exception);
+        }
+
+        private void Setup_UsersRepo_GetAllUserResources_Default(IEnumerable<UserResource> returnVal)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAllUserResources(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>())
+            ).ReturnsAsync(returnVal);
+        }
+
+        private void Setup_UsersRepo_GetAUserResource_ThrowsException(System.Exception exception)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAUserResource(It.IsAny<string>())
+            ).Throws(exception);
+        }
+
+        private void Setup_UsersRepo_GetAUserResource_Default(UserResource returnVal)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.GetAUserResource(It.IsAny<string>())
+            ).ReturnsAsync(returnVal);
+        }
+
+        private void Setup_UsersRepo_UpdateAUser_ThrowsException(System.Exception exception)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.UpdateAUser(It.IsAny<UserSummary>(), It.IsAny<Location>())
+            ).Throws(exception);
+        }
+
+        private void Setup_UsersRepo_UpdateAUser_Default(string returnVal)
+        {
+            _mockUsersRepo.Setup(
+                repo => repo.UpdateAUser(It.IsAny<UserSummary>(), It.IsAny<Location>())
+            ).ReturnsAsync(returnVal);
+        }
+
+        /********** Helper function for Projects repo setup **********/
+            // projectsRepo.GetAllProjectResourcesOfUser
+
+        /********** Helper function for Positions repo setup **********/
+            // positionsRepo.GetPositionsOfUser
+
+        /********** Helper function for Locations repo setup **********/
+
+        /********** Helper function for Disciplines repo setup **********/
+            // disciplinesRepo.GetUserDisciplines
+            // disciplinesRepo.InsertResrouceDiscipline
+            // disciplinesRepo.DeleteResourceDiscipline
+
+        /********** Helper function for Skills repo setup **********/ 
+            // skillsRepo.GetUserSkills
+            // skillsRepo.InsertResourceSkill
+            // skillsRepo.DeleteResourceSkill
+        
+        /********** Helper function for OutOfOffice repo setup **********/  
+            // outOfOfficeRepo.GetAllOutOfOFficeForUser
+            // outOfOfficeRepo.DeleteOutOfOffice
+            // outOfOfficeRepo.InsertOutOfOffice
+
+        /********** Tests for GetAllUsers **********/
+        [Fact]
+        public async void GetAllUsers_TryBlock_ReturnObjectResult()
+        {
+            var result = await _controller.GetAllUsers(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>());
+            Assert.IsType<ObjectResult>(result);
+        }
+
+        /********** Tests for GetAUser **********/
+        [Fact]
+        public async void GetAUser_TryBlock_ReturnObjectResult()
+        {
+            var result = await _controller.GetAUser(It.IsAny<string>());
+            Assert.IsType<ObjectResult>(result);
+        }
+
+        /********** Tests for UpdateUser **********/
+        [Fact]
+        public async void UpdateUser_TryBlock_ReturnObjectResult()
+        {
+            var result = await _controller.UpdateUser(It.IsAny<UserProfile>(), It.IsAny<string>());
+            Assert.IsType<ObjectResult>(result);
+        }
+    
+                /********** Tests for GetAUser **********/
+        [Fact]
+        public async void SearchUsers_TryBlock_ReturnObjectResult()
+        {
+            var result = await _controller.SearchUsers(It.IsAny<RequestSearchUsers>());
+            Assert.IsType<ObjectResult>(result);
+        }
     }
 }
