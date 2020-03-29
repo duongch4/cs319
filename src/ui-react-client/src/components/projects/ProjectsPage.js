8edea0529b609f9d00d60031a8ab3e8df94dfd29
Nicole <nicolekitner@gmail.com>
2020-03-29 15:33:05 -0400

    added search bar to projects page

--- src/ui-react-client/src/components/projects/ProjectsPage.js
+++ src/ui-react-client/src/components/projects/ProjectsPage.js
@@ -9,6 +9,7 @@ import AddIcon from '@material-ui/icons/Add';
 import { Link } from 'react-router-dom'
 import {CLIENT_DEV_ENV} from '../../config/config';
 import {UserContext, getUserRoles} from "../common/userContext/UserContext";
+import {Button} from "@material-ui/core";
 
 const _ProjectsPage = (props) => {
   const userRoles = getUserRoles(useContext(UserContext));
@@ -26,6 +27,10 @@ const _ProjectsPage = (props) => {
   }, [props.projects, props.loadProjects, userRoles]);
   return (
     <div className="activity-container">
+      <div className="form-row">
+            <input className="input-box" type="text" id="search" placeholder="Search"/>
+            <Button variant="contained" style={{backgroundColor: "#2c6232", color: "#ffffff", size: "small"}} disableElevation onClick={()=> this.saveFilter()}>Search</Button>
+        </div>
         <div className="title-bar">
           <h1 className="greenHeader">Manage Projects</h1>
           <div className="fab-container">
