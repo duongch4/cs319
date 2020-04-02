984e6e0d579e9ebae34763c4f235e8fbe03f81a4
Bang Chi Duong <chibangduong1993@gmail.com>
2020-04-02 02:58:28 -0700

    Changed JsonElement to Dictionary<string, int> for CommitmentMonlyHours; Fixed a bug in DeleteAllOpeningPositions in ProjectsRepo!! => will update this part on staging first for frontend to query

--- src/svc-dotnetcore3/svc-dotnetcore3/Controllers/ProjectsController.cs
+++ src/svc-dotnetcore3/svc-dotnetcore3/Controllers/ProjectsController.cs
@@ -284,6 +284,7 @@ namespace Web.API.Controllers
         [ProducesResponseType(typeof(InternalServerException), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateAProject([FromBody] ProjectProfile projectProfile)
         {
+            Log.Information("{@A}", projectProfile);
             if (projectProfile == null)
             {
                 var error = new BadRequestException("The given project is null / Request Body cannot be read");
@@ -406,6 +407,8 @@ namespace Web.API.Controllers
         [ProducesResponseType(typeof(InternalServerException), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UpdateAProject([FromBody] ProjectProfile projectProfile, string projectNumber)
         {
+            Log.Information("{@A}", projectProfile);
+
             if (projectProfile == null)
             {
                 var error = new BadRequestException("The given project profile is null / Request Body cannot be read");
