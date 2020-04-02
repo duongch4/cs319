1f24b7dfe76eee255c98e017c874d63813f275c4
kayetan6 <kayetan6@students.cs.ubc.ca>
2020-04-02 08:25:29 -0700

    Adjusted forecasting styling

--- src/ui-react-client/src/components/common/forecasting/Forecasting.js
+++ src/ui-react-client/src/components/common/forecasting/Forecasting.js
@@ -17,9 +17,9 @@ import Loading from '../Loading';
 import {UserContext, getUserRoles} from "../userContext/UserContext";
 
 class Forecasting extends Component {
-  state = {
-      users: null,
-      openingId: null,
+    state = {
+        users: null,
+        openingId: null,
         projectProfile: {
             projectSummary: {},
             projectManager: {},
@@ -29,91 +29,90 @@ class Forecasting extends Component {
         pending: true,
         masterlist: {},
         prevProps: null,
-  };
+    };
 
-  componentDidMount() {
-      this.setState({
-          ...this.state,
-          projectProfile: this.props.projectProfile,
-          openingId: this.props.match.params.positionID,
-          users: this.props.users,
-          pending: false
-      })
-  }
-  componentDidUpdate(prevProps){
-    this.state.prevProps = prevProps;
-  }
+    componentDidMount() {
+        this.setState({
+            ...this.state,
+            projectProfile: this.props.projectProfile,
+            openingId: this.props.match.params.positionID,
+            users: this.props.users,
+            pending: false
+        })
+    }
+    componentDidUpdate(prevProps){
+        this.state.prevProps = prevProps;
+    }
 
-  render() {
-    if (!this.state.pending) {
-      if(Object.keys(this.state.projectProfile).length === 0){
-        this.props.history.goBack();
-        return <div></div>;
-      }
-    let project_title = this.state.projectProfile.projectSummary ? this.state.projectProfile.projectSummary.title : "";
-    let users = this.state.users;
+    render() {
+        if (!this.state.pending) {
+            if(Object.keys(this.state.projectProfile).length === 0){
+                this.props.history.goBack();
+                return <div></div>;
+            }
+            let project_title = this.state.projectProfile.projectSummary ? this.state.projectProfile.projectSummary.title : "";
+            let users = this.state.users;
 
-    let openings = this.state.projectProfile.openings;
-    let selectedOpening = {};
+            let openings = this.state.projectProfile.openings;
+            let selectedOpening = {};
 
-    openings.forEach((opening, index) => {
-        if (opening.positionID == this.state.openingId) {
-            selectedOpening = opening;
+            openings.forEach((opening, index) => {
+                if (opening.positionID == this.state.openingId) {
+                    selectedOpening = opening;
+                }
+            });
+
+            let discipline_name = selectedOpening.discipline;
+            const project_number = this.state.projectProfile.projectSummary.projectNumber;
+            const userRoles = getUserRoles(this.context);
+
+            return (
+                <div>
+                    <div className="activity-container-no-padding">
+                        <div className="title-bar">
+                            <h2 className="blueHeader">Project: {project_title}</h2>
+                        </div>
+                        <div className="title-bar">
+                            <h1 className="greenHeader">{"Assign: " + discipline_name}</h1>
+                        </div>
+                    </div>
+                    <Search onDataFetched={this.handleResultChange}
+                            masterlist={this.state.masterlist}
+                            isAssignable={true}
+                            openingId={this.state.openingId}
+                            projectNumber={project_number}
+                            createAssignOpenings={(openingId, userId, utilization, user, userRoles) => this.props.createAssignOpenings(openingId, userId, utilization, user, userRoles)}/>
+                </div>
+            )
         }
-    });
+        else {
+            return (
+                <div className="activity-container">
+                    <Loading />
+                </div>
+            )
 
-    let discipline_name = selectedOpening.discipline;
-    const project_number = this.state.projectProfile.projectSummary.projectNumber;
-    const userRoles = getUserRoles(this.context);
-
-    return (
-      <div className="activity-container">
-          <div className="title-bar">
-          <h2 className="blueHeader">{project_title}</h2>
-          </div>
-          <div className="title-bar">
-            <h1 className="greenHeader">{"Assign: " + discipline_name}</h1>
-          </div>
-          <div>
-          <Search onDataFetched={this.handleResultChange}
-                    masterlist={this.state.masterlist}
-                    isAssignable={true}
-                    openingId={this.state.openingId}
-                    projectNumber={project_number}
-                    createAssignOpenings={(openingId, userId, utilization, user, userRoles) => this.props.createAssignOpenings(openingId, userId, utilization, user, userRoles)}/>
-          </div>
-      </div>
-    )
-  }
-  else {
-    return (
-   <div className="activity-container">
-     <Loading />
-   </div>
- )
-
-  }
-}
+        }
+    }
 }
 
 Forecasting.contextType = UserContext;
 
 const mapStateToProps = state => {
-  return {
-    users: state.users,
-    projectProfile: state.projectProfile,
-    openingId: state.openingId,
-    projectProfile: state.projectProfile,
-    pending: state.pending,
-  };
+    return {
+        users: state.users,
+        projectProfile: state.projectProfile,
+        openingId: state.openingId,
+        pending: state.pending,
+    };
 };
 
 const mapDispatchToProps = {
-  createAssignOpenings,
-  loadMasterlists
+    createAssignOpenings,
+    loadMasterlists
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(Forecasting);
