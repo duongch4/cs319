d1cdd73b65abbfe2e68aa90e46982850e98b188a
Nicole <nicolekitner@gmail.com>
2020-04-05 14:01:56 -0400

    added isunmounted check for set states that can try to exectute when user changes pages before completing

--- src/ui-react-client/src/components/users/UsersPage.js
+++ src/ui-react-client/src/components/users/UsersPage.js
@@ -12,6 +12,7 @@ import Select from 'react-select';
 class UsersPage extends Component {
   constructor(props) {
     super(props);
+    this._ismounted = false;
     this.initialState = {
       users: [],
       filters: null,
@@ -33,73 +34,88 @@ class UsersPage extends Component {
   }
 
   componentDidMount() {
+    this._ismounted = true;
     if (CLIENT_DEV_ENV) {
       this.props.loadMasterlists(["adminUser"]);
-      this.setState({
-        ...this.state,
-        masterlist: this.props.masterlist,
-      })
+      if (this._ismounted) {
+        this.setState({
+          ...this.state,
+          masterlist: this.props.masterlist,
+        })
+      }
     } else {
       const userRoles = getUserRoles(this.context);
       this.props.loadMasterlists(userRoles)
           .then(() => {
-            this.setState({
-              ...this.state,
-              masterlist: this.props.masterlist,
-            })
+            if (this._ismounted) {
+              this.setState({
+                ...this.state,
+                masterlist: this.props.masterlist,
+              })
+            }  
           });
       this.props.loadUsers(this.state.url.concat("&page=1"), userRoles)
       .then(()=> {
-        this.setState({
-          ...this.state,
-          users: this.props.users,
-          usersAll: [this.props.users],
-        }, () => (
-          this.state.users.length < 50 ? this.setState({...this.state, loading: false, doneLoading: true}) : this.getAll(userRoles, this.state.currPage, this.state.offset)
-        ));
+        if (this._ismounted) {
+          this.setState({
+            ...this.state,
+            users: this.props.users,
+            usersAll: [this.props.users],
+          }, () => (
+            this.state.users.length < 50 ? this.setState({...this.state, loading: false, doneLoading: true}) : this.getAll(userRoles, this.state.currPage, this.state.offset)
+          ));
+        } 
       })
       .catch(error => {
-        this.setState({
-          ...this.state,
-          noResults: true,
-          loading: false,
-        });
+          this.setState({
+            ...this.state,
+            noResults: true,
+            loading: false,
+          });
       });
     }
   }
 
   componentDidUpdate() {
-    if (!this.state.doneLoading && !this.state.loading && (Math.abs(this.state.lastPage - this.state.currPage) < 2)) {
-      this.setState({
-        ...this.state, 
-        loading: true,
-      }, () => this.getAll(getUserRoles(this.context), this.state.lastPage, this.state.offset));
-    }
+      if (!this.state.doneLoading && !this.state.loading && (Math.abs(this.state.lastPage - this.state.currPage) < 2)) {
+        if (this._ismounted) {
+          this.setState({
+            ...this.state, 
+            loading: true,
+          }, () => this.getAll(getUserRoles(this.context), this.state.lastPage, this.state.offset));
+        }
+      }
+  }
+
+  componentWillUnmount() {
+    this._ismounted = false;
   }
 
   restartLoad() {
     const userRoles = getUserRoles(this.context);
     this.props.loadUsers(this.state.url.concat("&page=1"), userRoles)
     .then(()=> {
-      this.setState({
-        ...this.state,
-        users: this.props.users,
-        usersAll: [this.props.users],
-        reloading: false,
-        noResultsNextPage: false,
-      }, () => (
-        this.state.users.length < 50 ? this.setState({...this.state, loading: false, doneLoading: true}) : this.getAll(userRoles, this.state.currPage, this.state.offset)
-      ));
+      if (this._ismounted) {
+        this.setState({
+          ...this.state,
+          users: this.props.users,
+          usersAll: [this.props.users],
+          reloading: false,
+          noResultsNextPage: false,
+        }, () => (
+          this.state.users.length < 50 ? this.setState({...this.state, loading: false, doneLoading: true}) : this.getAll(userRoles, this.state.currPage, this.state.offset)
+        ));
+      }
     })
     .catch(error => {
-      this.setState({
-        ...this.state,
-        noResults: true,
-        loading: false,
-        reloading: false,
-        noResultsNextPage: false,
-      });
-    });
+        this.setState({
+          ...this.state,
+          noResults: true,
+          loading: false,
+          reloading: false,
+          noResultsNextPage: false,
+        });
+    }); 
   }
 
   getAll(userRoles, currPage, offset) {
@@ -111,26 +127,27 @@ class UsersPage extends Component {
         var url = this.state.url.concat("&page=").concat(newPage);
         this.props.loadUsers(url, userRoles)
         .then(()=> {
-          this.setState({
-            ...this.state,
-              usersAll: [...this.state.usersAll, this.props.users],
-              noResults: false,
-              loading: true,
-              lastPage: currPage,
-              noResultsNextPage: false,
-          }, () => this.getAll(userRoles, newPage, offset));
-        })
-        .catch(error => {
-          this.setState({
-            ...this.state,
-            noResultsNextPage: false,
-            loading: false,
-            lastPage: currPage,
-            doneLoading: true,
-            });
-        });
-        }
-      } else {
+          if (this._ismounted) {
+            this.setState({
+              ...this.state,
+                usersAll: [...this.state.usersAll, this.props.users],
+                noResults: false,
+                loading: true,
+                lastPage: currPage,
+                noResultsNextPage: false,
+            }, () => this.getAll(userRoles, newPage, offset));
+          }})
+          .catch(error => {
+              this.setState({
+                ...this.state,
+                noResultsNextPage: false,
+                loading: false,
+                lastPage: currPage,
+                doneLoading: true,
+                });
+          });
+          }
+          } else {
         // stops loading after it loads 10 pages
           this.setState({
             ...this.state,
@@ -139,44 +156,44 @@ class UsersPage extends Component {
             offset: this.state.offset + 1,
             lastPage: currPage,
         });
-      }
-    }
+    }       
+  }
   
     toNextPage = () => {
       var new_page = this.state.currPage + 1;
       var page_index = this.state.currPage;
       if (this.state.usersAll[page_index] !== undefined && !this.state.noResultsNextPage) {
-        this.setState({
+          this.setState({
             ...this.state,
             users: this.state.usersAll[page_index],
             currPage: new_page,
-        })
+        }) 
       } else {
-        this.setState({
-          ...this.state,
-          noResultsNextPage: true,
-      })
-      } 
+          this.setState({
+            ...this.state,
+            noResultsNextPage: true,
+        })
+        }  
     }
   
   toPrevPage = () => {
     var new_page = this.state.currPage - 1;
     var page_index = new_page - 1;
-    this.setState({
-      ...this.state,
-      users: this.state.usersAll[page_index],
-      currPage: new_page,
-    })
+      this.setState({
+        ...this.state,
+        users: this.state.usersAll[page_index],
+        currPage: new_page,
+      })
   }
 
   sortUsers = (e) => {
     var sortBy = e.value;
     if (sortBy === "name-AZ"){
-      this.setState({
-        ...this.initialState,
-        reloading: true,
-        url: "?&orderKey=lastName&order=asc",
-      }, () => this.restartLoad());
+        this.setState({
+          ...this.initialState,
+          reloading: true,
+          url: "?&orderKey=lastName&order=asc",
+        }, () => this.restartLoad());
     } else if (sortBy === "name-ZA") {
       this.setState({
         ...this.initialState,
@@ -200,7 +217,7 @@ class UsersPage extends Component {
         ...this.initialState,
         reloading: true,
         url: "?&orderKey=province&order=asc",
-      }, () => this.restartLoad());       
+      }, () => this.restartLoad());     
     } else if (sortBy === "locations-ZA") {
       this.setState({
         ...this.initialState,
